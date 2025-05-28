4ae28f16864a4694e9ce3e1e1f0d866ed92afe20
JorLoCo <21460224@colima.tecnm.mx>
2025-05-28 11:24:55 -0600

    webservice.ts

--- src/webservice.ts
+++ src/webservice.ts
@@ -1,10 +1,12 @@
 import { ScannedCode } from './models';
 import axios from 'axios';
 
+const hostApi = 'http://localhost:3000';
+
 export async function getAll(): Promise<ScannedCode[]>
 {
     try{
-        const response = await fetch('http://localhost:3000/codigos');
+        const response = await fetch(`${hostApi}/codigos`);
         const data = await response.json();
         return data as ScannedCode[];
     }catch (error) {
@@ -14,19 +16,51 @@ export async function getAll(): Promise<ScannedCode[]>
 }
 export async function getById(id: string): Promise<ScannedCode|null>
 {
-    try{
-        const response = await axios.get(`http://localhost:3000/codigos/${id}`);
-        return response.data as ScannedCode;
-    }catch (error) {
-        console.error('Error retrieving resource:', error);
-        return null;
+  try{
+    const response = await axios.get(`${hostApi}/codigos/${id}`);
+    if (response.status >= 400) {
+      console.error(response.data);
+      return null;
     }
+    return response.data as ScannedCode;
+  }catch (error) {
+    console.error('Error retrieving resource:', error);
+    return null;
+  }
 }
-export async function create(code: ScannedCode)
-{
+export async function create(code: ScannedCode) {
+  try {
+    const response = await axios.post(`${hostApi}/codigos`, code, {
+      headers: {
+        'Content-Type': 'application/json',
+        mode: 'cors'
+      }
+    });
+    console.log(response);
+    if (response.status >= 500) {
+      console.error(response.data);
+    }
+  } catch (error) {
+    console.error('Error creating resource', error);
+  }
+}
+export async function deleteById(id: string): Promise<boolean> {
+  try {
+    const response = await axios.delete(`${hostApi}/codigos/${id}`);
+    if (response.status >= 400) {
+      console.error(response.data);
+      return false;
+    }
+    return true;
+  } catch (error) {
+    console.error('Error deleting resource', error);
+    return false;
+  }
+};
 
-}
-export async function deleteById(id: string)
-{
-
-}
+export const api = {
+  getAll,
+  getById,
+  create,
+  deleteById,
+};
